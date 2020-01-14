74bdbab53fe31e5c71a9891ec1b93fd2515881b8
LOWERCASE <52083329+LOWERCASE-repo@users.noreply.github.com>
2020-01-14 09:53:15 -0500

    c++ is the boomer lang

---
+++ loops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+   for(int i=1; i<=6; i++){
+      cout<<"Value of variable i is: "<<i<<endl;
+   }
+   int i=1;
+   while(i<=6){
+      cout<<"Value of variable i is: "<<i<<endl; i++;
+   }
+   return 0;
+}
